97e8ac186e4eecd25dfbb9ed73bfa5643516f00e
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-04-03 15:21:57 -0700

    Add flow for merge sort algorithm

---
+++ lib/merge_sort.rb
@@ -0,0 +1,8 @@
+#Take a list as an array
+#Divide the list into two arrays
+#Keep doing it until you have individual items in their own arrays
+#Take two arrays, read the first element of each.
+#Append the smaller of the two into a larger array, then the larger
+#Repeat the second element and so on.
+#Return the sorted array
+
