9a69540fca75ea822c0f63c87da286fd3b5713c3
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-04-03 13:06:49 -0700

    Add comments for fibonacci non-recursive flow

---
+++ lib/fibonacci.rb
@@ -0,0 +1,6 @@
+#Fibonacci sequence non-recursive
+#take an input n that represents the nth number of the Fibonacci sequence
+#Start with n = 0 -> 0 and n = 1 -> 1 in an array.
+#Add the last two numbers of this array and append it to itself
+#Loop this n - 2 times to get an array of length n.
+#Return the array
