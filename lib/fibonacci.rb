fbb18f62b903b0e6743c42688db2f40d1e1ba151
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-04-03 14:33:57 -0700

    Add recursive fibonacci function

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -13,5 +13,19 @@ def fibonacci(num)
     fibonacci_sequence
 end
 
-puts fibonacci(8)
+#puts fibonacci(8)
 
+#fibonacci sequence recursive
+#take an input n that represents the nth number of the sequence
+#and an input sequence with a default of [0, 1]
+#Start from bottom to top. Keep adding the last two digits until the array is num length
+
+def fib_recursive(num, sequence = [0, 1])
+    if sequence.length == 8
+        return sequence
+    else
+        fib_recursive(num, sequence.push(sequence[-1] + sequence[-2]))
+    end
+end
+
+puts fib_recursive(8)
